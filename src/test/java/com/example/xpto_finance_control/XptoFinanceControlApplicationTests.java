3d69da6b40059f763fd46e76ae03cc0ec4346e83
RodolfoXYZ <contato.rodolforodrigo@gmail.com>
2025-05-06 23:58:52 -0300

    Inicializando projeto com Spring Initializr

---
+++ src/test/java/com/example/xpto_finance_control/XptoFinanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xpto_finance_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XptoFinanceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
